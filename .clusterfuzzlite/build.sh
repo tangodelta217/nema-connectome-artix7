68b5fd4f2639db6ed6b845f956ca40ffb9d4656f
Mariano Millananco <mariano.millananco@gmail.com>
2026-03-03 12:38:48 +0100

    ci(cflite): rely on base-builder atheris to satisfy pinned-deps

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -eu
 
-# Ensure runtime dependencies for python fuzz targets are available.
-python3 -m pip install --no-cache-dir "atheris==3.0.0"
-
 # Use the canonical OSS-Fuzz/ClusterFuzzLite python packaging helper so
 # run_fuzzers can execute packaged targets with dependencies.
 for fuzzer in $(find "$SRC/fuzz" -name '*_fuzzer.py'); do
